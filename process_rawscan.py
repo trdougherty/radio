805510aaaa53d7cc058a074a88493106b897b1c3
trdougherty <trd4@me.com>
2019-12-19 16:53:44 -0500

    Processing and compressing data

--- process_rawscan.py
+++ process_rawscan.py
@@ -1,19 +1,24 @@
 import sys
 import os
 import pandas as pd
+import json
+import compress_json
 
-from compression import compress
+from compression import convert_json
 
 saving = "data"
 
 filename = sys.argv[1] #If this fails it means that the process was involved improperly
 scan = pd.read_csv(filename, delimiter=",", names=["Date","Time","hz_low","hz_high","hz_bin","n_samples","db1","db2","db3","db4","db5"])
-scan = compress(scan)
 
-save_name = os.path.splitext(filename)[0] + ".pkl"
+# This gets the base name for the file
+file_base = os.path.splitext(filename)[0]
+
+# Meat and bones of the processing
+json_scan = convert_json(scan)
 
 if not os.path.exists(saving):
     os.makedirs(saving)
 
-scan.to_pickle(saving+"/"+save_name)
+compress_json.dump(json_scan, saving+"/"+file_base+".json.gz")
 os.remove(filename)
