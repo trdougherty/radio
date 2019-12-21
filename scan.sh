460c042fef1dc067607c6947fc266bcdbf132d01
trdougherty <trd4@me.com>
2019-12-21 11:26:31 -0500

    Incorporating option for edge computing

--- scan.sh
+++ scan.sh
@@ -3,14 +3,11 @@ source .env
 
 # Shows the time in universal formatting
 D=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-temp_filename=$D"_sweep.txt"
+temp_filename=$D".txt"
 
 # gets the temp directory ready to roll
-echo $temp_filename
 mkdir -p $TEMP_DIR
 timeout --signal=SIGQUIT 5 hackrf_sweep -1 -r $TEMP_DIR/$temp_filename 2> bash_error.txt
-status=$?
-echo $status
 
 if [ -s $TEMP_DIR/$temp_filename ];
 then
@@ -19,5 +16,5 @@ then
 	python process_rawscan.py $TEMP_DIR/$temp_filename 2> python_error.txt
 else	
 	echo "Process failed."
-	# rm temp/$temp_filename
+	rm $TEMP_DIR/$temp_filename
 fi
