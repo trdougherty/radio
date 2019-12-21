6b0774ec0cd5b08244f3531ebf36f72c5e031702
trdougherty <trd4@me.com>
2019-12-20 20:28:49 -0500

    Error handling in main scan code

--- scan.sh
+++ scan.sh
@@ -8,7 +8,7 @@ temp_filename=$D"_sweep.txt"
 # gets the temp directory ready to roll
 echo $temp_filename
 mkdir -p $TEMP_DIR
-timeout --signal=SIGQUIT 5 hackrf_sweep -1 -r $TEMP_DIR/$temp_filename 2> error.txt
+timeout --signal=SIGQUIT 5 hackrf_sweep -1 -r $TEMP_DIR/$temp_filename 2> bash_error.txt
 status=$?
 echo $status
 
@@ -16,7 +16,7 @@ if [ ! -s $TEMP_DIR/$temp_filename ];
 then
 	echo "Processing scan..."
 	# Process that text into some kind of meaningful data representation
-	python process_rawscan.py $TEMP_DIR/$temp_filename
+	python process_rawscan.py $TEMP_DIR/$temp_filename 2> python_error.txt
 else	
 	echo "Process failed."
 	# rm temp/$temp_filename
