59e714645f468d0a2394d7e03ef3c4cc5179ff3a
trdougherty <trd4@me.com>
2019-12-22 02:17:35 -0500

    adding some funky logic around managing the time with the gps

--- run.sh
+++ run.sh
@@ -3,11 +3,24 @@ yell() { echo "$0: $*" >&2; }
 die() { yell "$*"; exit 111; }
 try() { "$@" || die "cannot $*"; }
 
+# Check if we have internet
+CON=$(ping -c 1 google.com | wc -l)
+if [ $CON -eq 0 ]; then
+    RES=$(python -c 'import gps_scan; from gps_scan import get_time; print get_time()')
+    if [ $RES -ne "None" ]; then
+    ## Set time here
+    # sudo date -s $RES
+    CON=1
+    fi
+fi
+
 # Starts mounting process
 try(){
     source .env
     bash mount_storage.sh 2> storage_error.txt
 }
+
+
 
 # Now it checks for the radio - if it exists
 if [ !$(lsusb | grep HackRF | wc -l) ]
