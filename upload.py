4ad4c366d70f8afa7b35c946bda6a16ef30a55ae
trdougherty <trd4@me.com>
2019-12-26 15:03:00 -0500

    Making treads into symmetric encoder

--- upload.py
+++ upload.py
@@ -4,7 +4,12 @@ import requests
 
 import dotenv
 dotenv.load_dotenv(verbose=True)
+
+# Local terms
+from encoder import encoder, string_normalization
+
 remote = os.getenv('REMOTE')
+public_keyname = os.getenv('PUBLIC_KEY')
 
 def upload(storage):
     try:
@@ -12,7 +17,16 @@ def upload(storage):
         with open(full_path, 'r') as f:
             d = json.load(f)
         
-        assert d
+        assert d # This validates that we have something here
+        assert isinstance(d, dict) # Validates that we have a dictionary
+        
+        d_string = json.dumps(d)
+        encoded = encoder(d_string, public_keyname)
+        
+        message = {
+            'key' : public_keyname,
+            'encrypted_data' : encoded 
+        }
         req = requests.post(remote, json=d)
         print req
         if (req.status_code == 200): #aka data was successfully recieved and interpreted without a problem
@@ -20,14 +34,14 @@ def upload(storage):
             #make the lights green or something
             return;
         # Else - raise some red light saying that the file was not properly uploaded
-    except requests.exceptions.RequestException as e:
-        print e
-    except AssertionError:
-        print "JSON term is null - could not be loaded from data"
-    except IOError:
-        print "Could not open file."
-    except ValueError:
-        print "Error with JSON interpretation."
+    # except requests.exceptions.RequestException as e:
+    #     print e
+    # except AssertionError:
+    #     print "JSON term is null - could not be loaded from data"
+    # except IOError:
+    #     print "Could not open file."
+    # except ValueError:
+    #     print "Error with JSON interpretation."
     except Exception as e:
         print "EXCEPTION:"
         print e.message, e.args
