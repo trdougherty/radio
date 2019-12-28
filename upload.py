264a472d0f854d95e0e676be18e55f1e9fdfdda2
trdougherty <trd4@me.com>
2019-12-27 23:04:58 -0500

    Building two part encryption system

--- upload.py
+++ upload.py
@@ -19,32 +19,28 @@ def upload(storage):
         
         assert d # This validates that we have something here
         assert isinstance(d, dict) # Validates that we have a dictionary
+        print "Dictionary validated. Continuing."
         
         d_string = json.dumps(d)
         encoded = encoder(d_string, public_keyname)
-        
-        message = {
-            'key' : public_keyname,
-            'encrypted_data' : encoded 
-        }
-        req = requests.post(remote, json=d)
+        print encoded['aes_encrypted']
+        req = requests.post(remote, json=encoded)
         print req
         if (req.status_code == 200): #aka data was successfully recieved and interpreted without a problem
             #os.remove(full_path)
             #make the lights green or something
             return;
         # Else - raise some red light saying that the file was not properly uploaded
-    # except requests.exceptions.RequestException as e:
-    #     print e
-    # except AssertionError:
-    #     print "JSON term is null - could not be loaded from data"
-    # except IOError:
-    #     print "Could not open file."
-    # except ValueError:
-    #     print "Error with JSON interpretation."
+    except requests.exceptions.RequestException as e:
+        print "Reuqest exception! Failed with error: ",e
+    except AssertionError as e:
+        print "JSON term is null - could not be loaded from data. Failed with error: ", e
+    except IOError as e:
+        print "Could not open file. Failed with error: ", e
+    except ValueError as e:
+        print "Error with JSON interpretation. Failed with error: ", e
     except Exception as e:
-        print "EXCEPTION:"
-        print e.message, e.args
+        print "EXCEPTION: ",e.message, e.args
         
 if __name__ == "__main__":
     storage = 'data/storage/2019-12-24T05:27:02Z.json'
