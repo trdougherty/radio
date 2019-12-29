f9bf8c087d3737f8a83a94eee961919100d3aadb
trdougherty <trd4@me.com>
2019-12-29 14:57:24 -0500

    Sym encoder

--- encoder.py
+++ encoder.py
@@ -2,13 +2,17 @@ from __future__ import unicode_literals
 
 import os
 import json
+import binascii
 
+from random import SystemRandom
 import base64
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 
 from os.path import join, dirname, realpath, splitext
 
@@ -16,20 +20,62 @@ from dotenv import load_dotenv
 load_dotenv(verbose=False)
 
 def string_normalization(filename):
-    filename = filename + '.pub.pem' if not filename.endswith('.pub.pem') else filename
+    filename = filename + '.pem' if not filename.endswith('.pem') else filename
     return join(dirname(realpath(__file__)), filename)
 
-def sync_encoder(message):
-    key = Fernet.generate_key()
-    cipher_suite = Fernet(key)
-    encrypted = cipher_suite.encrypt(message)
+def sym_encoder(message, keyname):
+    # if not password:
+    #     alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    #     password = "".join(SystemRandom().choice(alphabet) for _ in range(40))
+        
+    # password_bytes = password.encode('utf-8')
+
+    # # GENERATE random salt (needed for PBKDF2HMAC)
+    # salt = os.urandom(64)
+
+    # # DERIVE key (from password and salt)
+    # kdf = PBKDF2HMAC(
+    #     algorithm=hashes.SHA512(),
+    #     length=32,
+    #     salt=salt,
+    #     iterations=10000,
+    #     backend=default_backend()
+    # )
+    # key = kdf.derive(password_bytes)
+
+    # # GENERATE random nonce (number used once)
+    # nonce = os.urandom(12)
+
+    # # ENCRYPTION
+    # aesgcm = AESGCM(key)
+    # cipher_text_bytes = aesgcm.encrypt(
+    #     nonce=nonce,
+    #     data=message.encode('utf-8'),
+    #     associated_data=None
+    # )
+    # # CONVERSION of raw bytes to BASE64 representation
+    # cipher_text = base64.urlsafe_b64encode(cipher_text_bytes)
+    
+    # # Dumping the data here will pass it as a string, which we need to encrypt everything asymetrically
+    # return {
+    #     'key': key,
+    #     'nonce': nonce,
+    #     'data_encrypted': cipher_text
+    # }
+    full_keyname = string_normalization(keyname)
+    with open(full_keyname, 'rb') as f:
+        key = f.read()
+    
+    # key = Fernet.generate_key()
+    enc = Fernet(key)
+    encrypted = json.dumps(enc.encrypt(message)).encode('utf-8')
     
     return {
-        'aes_string':key,
+        'key':keyname,
         'data_encrypted':encrypted
     }
     
-def async_encoder(package, public_key):
+def asym_encoder(package, public_key):
     public_key = string_normalization(public_key)
     with open(public_key, "rb") as key_file:
         public_key = serialization.load_pem_public_key(
@@ -45,19 +91,18 @@ def async_encoder(package, public_key):
             label=None
         )
     )
-    return base64.standard_b64encode(encrypted)
+    return encrypted
 
-def encoder(message, public_key):
-    sync_encrypted = sync_encoder(message) # This encrypts the message as part of the payload
-    aes_string = sync_encrypted['aes_string']
-    
-    # The encrypted data
-    data_encrypted = sync_encrypted['data_encrypted']
-    aes_encrypted = async_encoder(aes_string, public_key)
-    
-    return {
-        'key': public_key,
-        'aes_encrypted': aes_encrypted,
-        'data_encrypted': data_encrypted
-    }
+def encoder(message, key):
+    sym_encrypted = sym_encoder(message, key) # This encrypts the message as part of the payload
+    # key = base64.b64encode(sym_encrypted['aes_key'])
+    # # sym_package = json.dumps({
+    # #     'key':key,
+    # #     'nonce':nonce
+    # # })
+    # # The encrypted data
+    # data_encrypted = sym_encrypted['data_encrypted']
+    # sym_package_encrypted = asym_encoder(key, public_key)
+    # print "Type of encoding: ",type(sym_package_encrypted)
+    return sym_encrypted
     
