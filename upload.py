fa53de02751b622fc9a2232f2043a1b4c239c47d
trdougherty <trd4@me.com>
2020-01-31 12:57:40 -0800

    upload.py

--- upload.py
+++ upload.py
@@ -36,47 +36,46 @@ def strip_right(text, suffix):
 def upload(directory, filename):
     filename_full = os.path.abspath(os.path.join(directory, filename))
     error_fullpath = os.path.abspath(os.path.join(os.path.dirname(__file__),error_dir))
-    try:
-        # This is where we gather the upload data
-        with open(filename_full, 'r') as f:
-            d = json.load(f)
-        
-        assert d # This validates that we have something here
-        assert isinstance(d, dict) # Validates that we have a dictionary
-        
-        name_kernel = strip_right(filename, '.json')
-        
-        d.update({ 'name': name_kernel })
-        d_string = json.dumps(d)
-        encoded = encoder(d_string, public_keyname)
-        req = requests.post(remote, json=encoded)
-        if (req.status_code == 200): #aka data was successfully recieved and interpreted without a problem
-            if gpio_bool:
-                upload_led.on()
-                time.sleep(1)
-                upload_led.off()
-            os.remove(filename_full)
-            return
-        # We got rate limited - need to wait for next upload
-        elif (req.status_code == 429):
-            if gpio_bool: error_led.on()
-            time.sleep(10)
-            if gpio_bool: error_led.off()
-            return
-        else:
-            if gpio_bool: error_led.on()
-            if not os.path.isdir(error_fullpath): os.makedirs(error_fullpath)
-            os.rename(filename_full, os.path.join(error_fullpath, filename))
-            time.sleep(3)
-            if gpio_bool: error_led.off()
-            return
-    except requests.exceptions.RequestException as e:
-        print("Reuqest exception! Failed with error: ",e)
-    except AssertionError as e:
-        print("JSON term is null - could not be loaded from data. Failed with error: ", e)
-    except IOError as e:
-        print("Could not open file. Failed with error: ", e)
-    except ValueError as e:
-        print("Error with JSON interpretation. Failed with error: ", e)
-    except Exception as e:
-        print("EXCEPTION: ", e)
+    # This is where we gather the upload data
+    with open(filename_full, 'r') as f:
+        d = json.load(f)
+    
+    assert d # This validates that we have something here
+    assert isinstance(d, dict) # Validates that we have a dictionary
+    
+    name_kernel = strip_right(filename, '.json')
+    
+    d.update({ 'name': name_kernel })
+    d_string = json.dumps(d)
+    encoded = encoder(d_string, public_keyname)
+    req = requests.post(remote, json=encoded)
+    if (req.status_code == 200): #aka data was successfully recieved and interpreted without a problem
+        if gpio_bool:
+            upload_led.on()
+            time.sleep(1)
+            upload_led.off()
+        os.remove(filename_full)
+        return
+    # We got rate limited - need to wait for next upload
+    elif (req.status_code == 429):
+        if gpio_bool: error_led.on()
+        time.sleep(10)
+        if gpio_bool: error_led.off()
+        return
+    else:
+        if gpio_bool: error_led.on()
+        if not os.path.isdir(error_fullpath): os.makedirs(error_fullpath)
+        os.rename(filename_full, os.path.join(error_fullpath, filename))
+        time.sleep(3)
+        if gpio_bool: error_led.off()
+        return
+    # except requests.exceptions.RequestException as e:
+    #     print("Reuqest exception! Failed with error: ",e)
+    # except AssertionError as e:
+    #     print("JSON term is null - could not be loaded from data. Failed with error: ", e)
+    # except IOError as e:
+    #     print("Could not open file. Failed with error: ", e)
+    # except ValueError as e:
+    #     print("Error with JSON interpretation. Failed with error: ", e)
+    # except Exception as e:
+    #     print("EXCEPTION: ", e)
