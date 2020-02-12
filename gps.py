3898d2f613b8898e7039215ffea4682e24ef62e4
trdougherty <trd4@me.com>
2020-02-11 22:43:23 -0800

    Adding class structure to gps and hoping for some redundancy;

--- gps.py
+++ gps.py
@@ -9,33 +9,35 @@ import adafruit_gps
 # for a computer, use the pyserial library for uart access
 import serial
 
-def connection():
-    uart = serial.Serial("/dev/serial0", baudrate=9600, timeout=7000)
-    #uart = busio.UART(board.TX, board.RX, baudrate=9600, timeout=10)
-    gps = adafruit_gps.GPS(uart, debug=True)
-    return ( uart, gps )
+class GPS():
+    def __init__(self):
+        ( self.uart, self.gps ) = self.setup()
+        
+    def connection(self):
+        uart = serial.Serial("/dev/serial0", baudrate=9600, timeout=7000)
+        #uart = busio.UART(board.TX, board.RX, baudrate=9600, timeout=10)
+        gps = adafruit_gps.GPS(uart, debug=True)
+        return ( uart, gps )
 
-def setup():
-    ( uart, gps ) = connection()  # Use UART/pyserial
-    gps.send_command(b'PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
-    time.sleep(2)
-    # Set update rate to once a second (1hz) which is what you typically want.
-    gps.send_command(b'PMTK220,300')
-    return ( uart, gps )
+    def setup(self):
+        ( uart, gps ) = self.connection()  # Use UART/pyserial
+        gps.send_command(b'PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+        time.sleep(2)
+        # Set update rate to once a second (1hz) which is what you typically want.
+        gps.send_command(b'PMTK220,300')
+        return ( uart, gps )
 
-# Main loop runs forever printing the location, etc. every second.
-def gather():
-    ( uart, gps ) = setup()
-    print(f'UART: {uart}')
-    print(f'GPS: {gps}')
-    gps.update()
-    if gps.has_fix:
-        print('GPS is fixed')
-        return {
-            "time": gps.timestamp_utc,
-            "lat": gps.latitude,
-            "lon": gps.longitude,
-            "alt": gps.altitude_m
-        }
-    else:
-        return None
+    # Main loop runs forever printing the location, etc. every second.
+    def gather(self):
+        self.gps.update()
+        if self.gps.has_fix:
+            print('GPS is fixed')
+            return {
+                "time": self.gps.timestamp_utc,
+                "lat": self.gps.latitude,
+                "lon": self.gps.longitude,
+                "alt": self.gps.altitude_m
+            }
+        else:
+            time.sleep(1)
+            return None
